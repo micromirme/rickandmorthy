a908fcf385f7cca1a8e88e6803a78e7fcfb17ca2
micromirme <zaharov.v@outlook.com>
2022-12-28 18:07:59 +0700

    Initial Commit

---
+++ RickAndMorthy/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RickAndMorthy
+//
+//  Created by Виталий Захаров on 28.12.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
